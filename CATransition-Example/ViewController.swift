caed26f00e643f2a56af50943292e489a5a7d625
leoiphonedev <aggarwalaman1985@gmail.com>
2018-08-30 20:35:34 +0530

    Pushing the source code for CATranstion example

--- CATransition-Example/ViewController.swift
+++ CATransition-Example/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblCounter: UILabel!
+    var counter = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,39 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func incrementCounter(_ sender: Any) {
+        lblCounter.layer.bottomAnimation(duration: 0.7)
+        counter = counter + 1
+        lblCounter.text = "\(counter)"
+    }
+    
+    @IBAction func devrementCounter(_ sender: Any) {
+        if counter > 0 {
+            lblCounter.layer.topAnimation(duration: 0.7)
+            counter = counter - 1
+            lblCounter.text = "\(counter)"
+        }
+    }
+}
 
+extension CALayer {
+    
+    func bottomAnimation(duration:CFTimeInterval) {
+        let animation = CATransition()
+        animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
+        animation.duration = duration
+        animation.type = kCATransitionPush
+        animation.subtype = kCATransitionFromTop
+        self.add(animation, forKey: kCATransitionPush)
+    }
+    
+    func topAnimation(duration:CFTimeInterval) {
+        let animation = CATransition()
+        animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
+        animation.duration = duration
+        animation.type = kCATransitionPush
+        animation.subtype = kCATransitionFromBottom
+        self.add(animation, forKey: kCATransitionPush)
+    }
 }
 
